dd4d966465e1ea719defe85f9411c9045fcc79d9
YQZ227 <91039782+YQZ227@users.noreply.github.com>
2021-09-20 21:32:00 -0400

    Add files via upload

---
+++ Server.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.net.*;
+class Server {
+	public static void main(String args[]) throws Exception {
+		if (args.length != 1) {
+			System.out.println("usage: java Server port");
+			System.exit(-1);
+		}
+		int port = Integer.parseInt(args[0]);
+		//Socket socket = null;
+		ServerSocket server = new ServerSocket(port);
+		System.out.println("listening on port " + port);
+		while(true) {
+			try {
+				/*
+				  YOUR CODE GOES HERE
+				  
+				  - add an inner loop to read requests from this connection
+				    repeatedly (client may reuse the connection for multiple
+				    requests)
+				  - for each request, compute an output and send a response
+				  - each message has a 4-byte header followed by a payload
+				  - the header is the length of the payload
+				    (signed, two's complement, big-endian)
+				  - the payload is a string (UTF-8)
+				  - the inner loop ends when the client closes the connection
+				*/
+
+
+				//- accept a connection from the server socket
+				System.out.println("Server started");
+				System.out.println("Waiting for a client ...");
+				Socket socket = server.accept();
+				System.out.println("Client accepted");
+
+				server.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
