286076ba193479ebaa10ea9f60701ab1c80de718
YQZ227 <91039782+YQZ227@users.noreply.github.com>
2021-09-27 22:59:39 -0400

    Delete run_server.sh

--- run_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export CLASSPATH=.:lib/*
-./build.sh
-
-echo --- Running
-RANDOM_PORT=`shuf -i 10000-10999 -n 1`
-echo randomly chose port $RANDOM_PORT
-java -Xmx1g Server $RANDOM_PORT
