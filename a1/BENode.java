db33e77f7827a1af0d3294b1ff8b154e070e3f03
YQZ227 <91039782+YQZ227@users.noreply.github.com>
2021-10-20 23:52:39 -0400

    Add files via upload

---
+++ a1/BENode.java
@@ -0,0 +1,74 @@
+import java.net.InetAddress;
+
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.TProcessorFactory;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.server.TThreadPoolServer;
+import org.apache.thrift.transport.*;
+
+
+public class BENode {
+	static Logger log;
+
+	public static void main(String [] args) throws Exception {
+		if (args.length != 3) {
+			System.err.println("Usage: java BENode FE_host FE_port BE_port");
+			System.exit(-1);
+		}
+
+		// initialize log4j
+		BasicConfigurator.configure();
+		log = Logger.getLogger(BENode.class.getName());
+
+		String hostFE = args[0];
+		int portFE = Integer.parseInt(args[1]);
+		int portBE = Integer.parseInt(args[2]);
+		log.info("Launching BE node on port " + portBE + " at host " + getHostName());
+
+		// launch Thrift server
+		BcryptService.Processor processor = new BcryptService.Processor<BcryptService.Iface>(new BcryptServiceHandler());
+		TServerSocket socket = new TServerSocket(portBE);
+		TThreadPoolServer.Args sargs = new TThreadPoolServer.Args(socket);
+		sargs.protocolFactory(new TBinaryProtocol.Factory());
+		sargs.transportFactory(new TFramedTransport.Factory());
+		sargs.processorFactory(new TProcessorFactory(processor));
+		sargs.maxWorkerThreads(64);
+
+		//add ourselves to FEnode's clientlist.
+		BcryptService.Client client;
+		TSocket sock = new TSocket(hostFE, portFE);
+		TTransport transport = new TFramedTransport(sock);
+		TProtocol protocol = new TBinaryProtocol(transport);
+		client = new BcryptService.Client(protocol);
+
+		while(true){
+			try {
+				transport.open();
+				break;
+			} catch(Exception e) {
+				System.out.println("DEBUG: No FE node, will try again in 1s");
+				Thread.sleep(1000);
+			}
+		}
+		client.addBE(getHostName(), String.valueOf(portBE));
+		transport.close();
+
+		//sargs.maxWorkerThreads(64);
+		TThreadPoolServer server = new TThreadPoolServer(sargs);
+		server.serve();
+
+	}
+
+	static String getHostName()
+	{
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (Exception e) {
+			return "localhost";
+		}
+	}
+}
